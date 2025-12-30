143ea1a1e40a29bed112062296a619de78109965
eewwqq1 <rlaehdrbs90@gmail.com>
2025-12-30 18:09:09 +0900

    클론 생성 및 2025-12-30 파이썬 til 생성

--- 2025-12/practice.py
+++ 2025-12/practice.py
@@ -2,8 +2,8 @@
 """
 질문 모음
 1.
-dict 자료 구조상 delete는 잘 쓰지 않으며
-컨스트럭팅, 키매핑, dto-entitiy, entitiy-dto 등의 방식을 우선한다. 이 말의 의미가 무엇인지요?
+dict 자료 구조상 delete를 바로 쓰지 않으며,
+컨스트럭팅, 키매핑, dto-entitiy, entitiy-dto 등의 방식을 우선한다. 이 말의 의미가 무엇 인지요?
 
 """
 
@@ -18,16 +18,60 @@ dict 자료 구조상 delete는 잘 쓰지 않으며
 # var2= var.pop()
 # # print(var2)
 #
-dict = {"key1":"value1", "key2":"value2", "key3":"value3"}
-print(dict.get("value1"))
-print(dict["key3"])
+# dict2 = {"key1":"value1", "key2":"value2", "key3":"value3"}
+# print(dict.get("value1"))
+# print(dict["key3"])
 
 #del dict["key3"]
-print(dict)
+# print(dict)
+#
+# for k in dict.keys():
+#     print(k)
+#
+# for k,v in dict.items():
+#     print(k,v)
+# #
+# del dict2
+# print(dict2)
 
-for k in dict.keys():
-    print(k)
+# val = "     안녕     "
+# val2 =val.strip()
+# print(val2)
+# val3 = val2.split("안")
+# print(val3)
+#
+# email = 'eewwqq1@naver.com'
+# user = email.split('@')
+# print(user[0])
+#
+# tup = ()
+# print(type(tup))
 
-for k,v in dict.items():
-    print(k,v)
+# tup = (1,"2",3)
+# print(tup[-1])
+# print(tup[1:-1])
+# print(len(tup[1:-1]))
+#
+# text = "안녕하세요"
+# print("안" in text)
 
+# aa = set()
+# dd = {1,2,3}
+#
+# print(type(aa))
+# print(type(dd))
+
+#
+# aa = set()
+# # aa.add("asasdad")
+# # aa.add("eccmmas")
+# # aa.add("csescsd")
+# # aa.add("ooadsqu")
+# print(aa)
+
+aa = set()
+aa.update([1,2,3,4])
+# aa.remove(5)
+aa.discard(5)
+print(type(aa.discard(5)))
+print(aa)
